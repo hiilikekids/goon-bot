f1d4d5f441a7982ddd1157e2bf3fd10d5594cb07
hiilikekids <beshiribrahim933@gmail.com>
2026-05-18 20:06:47 +0100

    Add files via upload

---
+++ commands/config/messages.js
@@ -0,0 +1,9 @@
+module.exports = {
+    DM_MESSAGES: {
+        warn: "⚠️ You received a warning. Please follow the server rules.",
+        timeout: "⏳ You have been timed out for repeated rule violations.",
+        kick: "👢 You were kicked from the server for breaking the rules multiple times.",
+        tempban: "⛔ You were temporarily banned. You will be unbanned automatically.",
+        permban: "You betrayed King Draga and his sacred laws, and therefore were executed by royal decree. In other words: HA! You got caught lacking and were BANNED LOOOOL"
+    }
+};
