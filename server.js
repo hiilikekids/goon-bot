f5e37e39ab62d2647f25bd8430d693ffa1e05c74
hiilikekids <beshiribrahim933@gmail.com>
2026-05-18 21:04:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const client = require("./index.js"); // your bot file
+const client = require("./index.js");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/status", (req, res) => {
-  res.json({ online: true, bot: client.user ? client.user.tag : null });
+  res.json({
+    online: true,
+    bot: client.user ? client.user.tag : null
+  });
 });
 
-app.listen(PORT, () => console.log("Website backend online on port " + PORT));
+app.listen(PORT, () => console.log("Backend online on port " + PORT));
